a5ed763ce11dbb5d7ad2614a23b09200b9a8951f
beezee <brian.zeligson@gmail.com>
2016-05-18 08:03:45 -0400

    randomly test null/undefined as None in jsdynamic CanParse

--- jsdynamic-typify/src/test/scala/CanParseProp.scala
+++ jsdynamic-typify/src/test/scala/CanParseProp.scala
@@ -4,37 +4,37 @@ import scala.scalajs.js
 import scala.scalajs.js.Dynamic.literal
 import scala.scalajs.js.annotation.JSExport
 import scala.scalajs.js.typify.parsedinstances._
-import org.scalacheck.Properties
+import org.scalacheck.{Gen, Properties}
 
 object MakeJsDynamic extends MakeParsed[js.Dynamic] {
 
   import implicits._
 
+  def none = Gen.oneOf(().asInstanceOf[js.Dynamic], null.asInstanceOf[js.Dynamic])
+                .sample.getOrElse(().asInstanceOf[js.Dynamic])
+
   def make[A](k: String, v: A)(implicit mp: MustParse[A]): js.Dynamic =
     mp match {
       case MPS => literal(k -> v)
-      case MPOS => MPOS(v).map(x => literal(k -> x)).getOrElse(().asInstanceOf[js.Dynamic])
+      case MPOS => MPOS(v).map(x => literal(k -> x)).getOrElse(none)
       case MPI => literal(k -> v)
-      case MPOI => MPOI(v).map(x => literal(k -> x)).getOrElse(().asInstanceOf[js.Dynamic])
+      case MPOI => MPOI(v).map(x => literal(k -> x)).getOrElse(none)
       case MPL => literal(k -> v)
-      case MPOL => MPOL(v).map(x => literal(k -> x)).getOrElse(().asInstanceOf[js.Dynamic])
+      case MPOL => MPOL(v).map(x => literal(k -> x)).getOrElse(none)
       case MPP => literal(k -> v)
-      case MPOP => MPOP(v).map(x => literal(k -> x)).getOrElse(().asInstanceOf[js.Dynamic])
+      case MPOP => MPOP(v).map(x => literal(k -> x)).getOrElse(none)
     }
+
   def to[A](v: A)(implicit mp: MustParse[A]): js.Dynamic =
     mp match {
       case MPS => v.asInstanceOf[js.Dynamic]
-      case MPOS => MPOS(v).map(_.asInstanceOf[js.Dynamic])
-                    .getOrElse(().asInstanceOf[js.Dynamic])
+      case MPOS => MPOS(v).map(_.asInstanceOf[js.Dynamic]).getOrElse(none)
       case MPI => v.asInstanceOf[js.Dynamic]
-      case MPOI => MPOI(v).map(_.asInstanceOf[js.Dynamic])
-                    .getOrElse(null.asInstanceOf[js.Dynamic])
+      case MPOI => MPOI(v).map(_.asInstanceOf[js.Dynamic]).getOrElse(none)
       case MPL => v.asInstanceOf[js.Dynamic]
-      case MPOL => MPOL(v).map(_.asInstanceOf[js.Dynamic])
-                    .getOrElse(().asInstanceOf[js.Dynamic])
+      case MPOL => MPOL(v).map(_.asInstanceOf[js.Dynamic]).getOrElse(none)
       case MPP => v.asInstanceOf[js.Dynamic]
-      case MPOP => MPOP(v).map(_.asInstanceOf[js.Dynamic])
-                    .getOrElse(null.asInstanceOf[js.Dynamic])
+      case MPOP => MPOP(v).map(_.asInstanceOf[js.Dynamic]).getOrElse(none)
     }
 }
 
