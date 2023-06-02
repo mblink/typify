2064d6c2f6cac9a566361863fc75043453380f0c
Matt Dziuban <mrdziuban@gmail.com>
2023-06-02 15:22:27 -0400

    Test out specialized instances of `Remover` and `Updater`, but they cause issues.

--- tagged/shared/src/main/scala-3/typify/labelled/Remover.scala
+++ tagged/shared/src/main/scala-3/typify/labelled/Remover.scala
@@ -8,22 +8,42 @@ trait Remover[T <: Tuple, K] {
   def apply(t: T): Out
 }
 
-object Remover {
-  type Aux[T <: Tuple, K, Out0] = Remover[T, K] { type Out = Out0 }
+sealed trait RemoverLP {
+  final type Aux[T <: Tuple, K, Out0] = Remover[T, K] { type Out = Out0 }
 
-  inline def apply[T <: Tuple, K](implicit r: Remover[T, K]): Aux[T, K, r.Out] = r
-  inline def apply[T <: Tuple, K](t: T, k: K)(implicit r: Remover[T, K]): r.Out = r(t)
-
-  given head[K, V, T <: Tuple]: Aux[(K ->> V) *: T, K, (V, T)] =
-    new Remover[(K ->> V) *: T, K] {
-      type Out = (V, T)
-      def apply(t: (K ->> V) *: T): (V, T) = (t.head, t.tail)
-    }
-
-  given tail[H, K, V, TI <: Tuple, TO <: Tuple](using r: Aux[TI, K, (V, TO)]): Aux[H *: TI, K, (V, H *: TO)] =
+  final given tailRemover[H, K, V, TI <: Tuple, TO <: Tuple](using r: Aux[TI, K, (V, TO)]): Aux[H *: TI, K, (V, H *: TO)] =
     new Remover[H *: TI, K] {
       type Out = (V, H *: TO)
       def apply(t: H *: TI): (V, H *: TO) =
         r(t.tail).pipe { case (v, to) => (v, t.head *: to) }
+    }
+}
+
+object Remover extends RemoverLP {
+  inline def apply[T <: Tuple, K](implicit r: Remover[T, K]): Aux[T, K, r.Out] = r
+  inline def apply[T <: Tuple, K](t: T, k: K)(implicit r: Remover[T, K]): r.Out = r(t)
+
+  /* TODO - revisit specializing Remover instances by generating this code:
+
+  println(0.to(100).map { i =>
+    val tps = 0.to(i - 1).map(j => s"A$j")
+    val tupleTpe = s"${tps.mkString(" *: ")}${if (tps.isEmpty) "" else " *: "}(K ->> V) *: T"
+    val rmTupleTpe = if (tps.isEmpty) "T" else s"${tps.mkString(" *: ")} *: T"
+s"""
+  given elem${i}Remover[${tps.mkString(", ")}${if (tps.isEmpty) "" else ", "}K, V, T <: Tuple]: Aux[$tupleTpe, K, (V, $rmTupleTpe)] =
+    new Remover[$tupleTpe, K] {
+      type Out = (V, $rmTupleTpe)
+      def apply(t: $tupleTpe): Out = {
+        val b = t.toArray.to(collection.mutable.Buffer)
+        (b.remove($i).asInstanceOf[V], Tuple.fromArray(b.to(Array)).asInstanceOf[$rmTupleTpe])
+      }
+    }"""
+  }.mkString("\n"))
+  */
+
+  given headRemover[K, V, T <: Tuple]: Aux[(K ->> V) *: T, K, (V, T)] =
+    new Remover[(K ->> V) *: T, K] {
+      type Out = (V, T)
+      def apply(t: (K ->> V) *: T): (V, T) = (t.head, t.tail)
     }
 }
--- tagged/shared/src/main/scala-3/typify/labelled/Updater.scala
+++ tagged/shared/src/main/scala-3/typify/labelled/Updater.scala
@@ -6,7 +6,7 @@ trait Updater[L <: Tuple, F] {
   def apply(l: L, f: F): Out
 }
 
-sealed trait UpdaterLP {
+sealed trait UpdaterLP0 {
   final type Aux[L <: Tuple, F, Out0] = Updater[L, F] { type Out = Out0 }
 
   final type Appended[T <: Tuple, F] <: Tuple = T match {
@@ -14,26 +14,42 @@ sealed trait UpdaterLP {
     case h *: t => h *: Appended[t, F]
   }
 
-  given updaterTupleAppend[T <: Tuple, F]: Aux[T, F, Appended[T, F]] =
+  final given appendUpdater[T <: Tuple, F]: Aux[T, F, Appended[T, F]] =
     new Updater[T, F] {
       type Out = Appended[T, F]
-      def apply(t: T, f: F): Appended[T, F] = Tuple.fromArray(t.toArray :+ (f: Any)).asInstanceOf[Appended[T, F]]
+      def apply(t: T, f: F): Appended[T, F] = Tuple.fromArray[Any](t.toArray :+ (f: Any)).asInstanceOf[Appended[T, F]]
     }
 }
 
-object Updater extends UpdaterLP {
-  inline def apply[L <: Tuple, F](implicit u: Updater[L, F]): Aux[L, F, u.Out] = u
-  inline def apply[L <: Tuple, F](l: L, f: F)(implicit u: Updater[L, F]): u.Out = u(l, f)
-
-  given updaterTupleHead[K, VI, VO, T <: Tuple]: Aux[(K ->> VI) *: T, K ->> VO, (K ->> VO) *: T] =
-    new Updater[(K ->> VI) *: T, K ->> VO] {
-      type Out = (K ->> VO) *: T
-      def apply(t: (K ->> VI) *: T, kv: K ->> VO): (K ->> VO) *: T = kv *: (t.tail: T)
-    }
-
-  given updaterTupleTail[H, K, V, T <: Tuple](using u: Updater[T, K ->> V] { type Out <: Tuple }): Aux[H *: T, K ->> V, H *: u.Out] =
+sealed trait UpdaterLP1 extends UpdaterLP0 {
+  final given tailUpdater[H, K <: Singleton, V, T <: Tuple](using u: Updater[T, K ->> V] { type Out <: Tuple }): Aux[H *: T, K ->> V, H *: u.Out] =
     new Updater[H *: T, K ->> V] {
       type Out = H *: u.Out
       def apply(t: H *: T, kv: K ->> V): H *: u.Out = t.head *: u(t.tail, kv)
+    }
+}
+
+object Updater extends UpdaterLP1 {
+  inline def apply[L <: Tuple, F](implicit u: Updater[L, F]): Aux[L, F, u.Out] = u
+  inline def apply[L <: Tuple, F](l: L, f: F)(implicit u: Updater[L, F]): u.Out = u(l, f)
+
+  /* TODO - revisit specializing Updater instances by generating this code:
+
+  println(0.to(100).map { i =>
+    val tps = 0.to(i - 1).map(j => s"A$j")
+    val tupleTpe = (v: String) => s"${tps.mkString(" *: ")}${if (tps.isEmpty) "" else " *: "}(K ->> $v) *: EmptyTuple"
+s"""
+  given elem${i}Updater[${tps.mkString(", ")}${if (tps.isEmpty) "" else ", "}K <: Singleton, VI, VO]: Aux[${tupleTpe("VI")}, K ->> VO, ${tupleTpe("VO")}] =
+    new Updater[${tupleTpe("VI")}, K ->> VO] {
+      type Out = ${tupleTpe("VO")}
+      def apply(t: ${tupleTpe("VI")}, f: K ->> VO): Out = Tuple.fromArray[Any](t.toArray.updated($i, f)).asInstanceOf[Out]
+    }"""
+  }.mkString("\n"))
+  */
+
+  given headUpdater[K, VI, VO, T <: Tuple]: Aux[(K ->> VI) *: T, K ->> VO, (K ->> VO) *: T] =
+    new Updater[(K ->> VI) *: T, K ->> VO] {
+      type Out = (K ->> VO) *: T
+      def apply(t: (K ->> VI) *: T, f: K ->> VO): Out = f *: t.tail
     }
 }
