7b53b965affc58c90122aacbc8a0ad360c976747
Matt Dziuban <mrdziuban@gmail.com>
2023-06-06 17:58:00 -0400

    Remove unused code.

--- tagged/shared/src/main/scala-3/typify/labelled/ValueAtKey.scala
+++
@@ -1,6 +0,0 @@
-package typify.labelled
-
-type ValueAtKey[T <: Tuple, K] = T match {
-  case (K ->> v) *: _ => v
-  case _ *: t => ValueAtKey[t, K]
-}
--- tuple/shared/src/main/scala-3/typify/tuple/IndexOf.scala
+++ tuple/shared/src/main/scala-3/typify/tuple/IndexOf.scala
@@ -7,10 +7,3 @@ type IndexOf0[T <: Tuple, E, I <: Int] <: Int = T match {
   case _ *: t => IndexOf0[t, E, compiletime.ops.int.S[I]]
   case EmptyTuple => -1
 }
-
-type IndexOfNoFallback[T <: Tuple, E] = IndexOfNoFallback0[T, E, 0]
-
-type IndexOfNoFallback0[T <: Tuple, E, I <: Int] <: Int = T match {
-  case E *: _ => I
-  case _ *: t => IndexOf0[t, E, compiletime.ops.int.S[I]]
-}
