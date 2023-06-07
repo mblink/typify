0781a157fa1b6cb2b431c113b23646f5f5f37c39
Matt Dziuban <mrdziuban@gmail.com>
2023-06-07 17:50:18 -0400

    Make `->>` an opaque type for `TranslucentTagged`.
    This is necessary to work around what I'm assuming is a bug in the scala 3 compiler -- the following code errors when it's simply an opaque type for `V`:
    
    ```scala
    import typify.tuple._
    import typify.record._
    
    val t = ("s" ->> "foo") *: ("i" ->> 3) *: EmptyTuple
    val s = Selector[("s" ->> String) *: ("i" ->> Int) *: EmptyTuple, "i"]
    // java.lang.ClassCastException: class java.lang.Integer cannot be cast to class java.lang.String
    ```

--- tuple/shared/src/main/scala-3/typify/record/RecordPackageAux.scala
+++ tuple/shared/src/main/scala-3/typify/record/RecordPackageAux.scala
@@ -4,7 +4,7 @@ package record
 import scala.language.implicitConversions
 
 trait RecordPackageAux {
-  final opaque type ->>[K, +V] = V
+  final opaque type ->>[K, +V] = tagged.TranslucentTagged[V, K]
   object ->> {
     implicit def convertToV[K, V](kv: K ->> V): V = kv
   }
@@ -12,7 +12,7 @@ trait RecordPackageAux {
   @inline final def label[K]: [V] => V => (K ->> V) = [v] => (v: v) => tagged.translucentTag[K](v)
 
   extension[K <: Singleton](k: K) {
-    inline final def ->>[V](v: V): K ->> V = v
+    inline final def ->>[V](v: V): K ->> V = tagged.translucentTag[K](v)
   }
 
   extension[K, V](kv: K ->> V) {
