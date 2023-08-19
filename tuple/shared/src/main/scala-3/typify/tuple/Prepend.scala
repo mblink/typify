ec8f2ec2e20dad9fdbab75816d9666a58db87d6f
Matt Dziuban <mrdziuban@gmail.com>
2023-08-19 14:01:06 -0400

    Simplify `Prepend` implementation.

--- tuple/shared/src/main/scala-3/typify/tuple/Prepend.scala
+++ tuple/shared/src/main/scala-3/typify/tuple/Prepend.scala
@@ -2,32 +2,14 @@ package typify.tuple
 
 trait Prepend[L, R] extends DepFn2[L, R]
 
-trait PrependLP1 {
-  final type Aux[L, R, O] = Prepend[L, R] { type Out = O }
+object Prepend {
+  type Aux[L, R, O] = Prepend[L, R] { type Out = O }
 
-  final given prependTupleCons[LH, LT <: Tuple, R <: Tuple, OT <: Tuple](
-    using pt: Prepend.Aux[LT, R, OT],
-  ): Prepend.Aux[LH *: LT, R, LH *: OT] =
-    new Prepend[LH *: LT, R] {
-      type Out = LH *: OT
-      def apply(l: LH *: LT, r: R): Out = l.head *: pt(l.tail, r)
-    }
-}
-
-trait PrependLP0 extends PrependLP1 {
-  final given prependEmptyTuple0[L <: Tuple, R <: EmptyTuple]: Prepend.Aux[L, R, L] =
-    new Prepend[L, R] {
-      type Out = L
-      def apply(l: L, r: R): L = l
-    }
-}
-
-object Prepend extends PrependLP0 {
   inline def apply[L, R](using p: Prepend[L, R]): Prepend.Aux[L, R, p.Out] = p
 
-  given prependEmptyTuple1[L <: EmptyTuple, R <: Tuple]: Prepend.Aux[L, R, R] =
+  given tuplePrepend[L <: Tuple, R <: Tuple]: Prepend.Aux[L, R, Tuple.Concat[L, R]] =
     new Prepend[L, R] {
-      type Out = R
-      def apply(l: L, r: R): R = r
+      type Out = Tuple.Concat[L, R]
+      def apply(l: L, r: R): Out = l ++ r
     }
 }
