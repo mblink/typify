9e467b68abb9c7bebf83744136c544965345c2f3
Matt Dziuban <mrdziuban@gmail.com>
2023-08-19 09:25:22 -0400

    Add `ToEither`.

---
+++ tuple/shared/src/main/scala-2.13/typify/tuple/ToEither.scala
@@ -0,0 +1,33 @@
+package typify.tuple
+
+trait ToEither[T] {
+  type Out
+}
+
+sealed trait ToEitherLP {
+  final type Aux[T, O] = ToEither[T] { type Out = O }
+
+  final implicit def toEitherTupleN[L, R <: Tuple](implicit r: ToEither[R]): ToEither.Aux[L *: R, Either[L, r.Out]] =
+    new ToEither[L *: R] {
+      type Out = Either[L, r.Out]
+    }
+}
+
+object ToEither extends ToEitherLP {
+  def apply[T <: Tuple](implicit e: ToEither[T]): ToEither.Aux[T, e.Out] = e
+
+  implicit val toEitherEmptyTuple: ToEither.Aux[EmptyTuple, Nothing] =
+    new ToEither[EmptyTuple] {
+      type Out = Nothing
+    }
+
+  implicit def toEitherTuple1[L]: ToEither.Aux[L *: EmptyTuple, Either[L, Nothing]] =
+    new ToEither[L *: EmptyTuple] {
+      type Out = Either[L, Nothing]
+    }
+
+  implicit def toEitherTuple2[L, R]: ToEither.Aux[L *: R *: EmptyTuple, Either[L, R]] =
+    new ToEither[L *: R *: EmptyTuple] {
+      type Out = Either[L, R]
+    }
+}
---
+++ tuple/shared/src/main/scala-3/typify/tuple/ToEither.scala
@@ -0,0 +1,23 @@
+package typify.tuple
+
+type ToEitherT[T <: Tuple] = T match {
+  case EmptyTuple => Nothing
+  case l *: EmptyTuple => Either[l, Nothing]
+  case l *: r *: EmptyTuple => Either[l, r]
+  case l *: r => Either[l, ToEitherT[r]]
+}
+
+trait ToEither[T] {
+  type Out
+}
+
+object ToEither {
+  type Aux[T, O] = ToEither[T] { type Out = O }
+
+  inline def apply[T <: Tuple](using e: ToEither[T]): ToEither.Aux[T, e.Out] = e
+
+  given toEitherNonEmptyTuple[T <: Tuple]: ToEither.Aux[T, ToEitherT[T]] =
+    new ToEither[T] {
+      type Out = ToEitherT[T]
+    }
+}
