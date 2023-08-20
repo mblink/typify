1b737fc7f9839a466acec45d863d65a0906eebb9
Matt Dziuban <mrdziuban@gmail.com>
2023-08-19 22:36:00 -0400

    Simplify `Remover`.

--- tuple/shared/src/main/scala-3/typify/record/Remover.scala
+++ tuple/shared/src/main/scala-3/typify/record/Remover.scala
@@ -15,22 +15,20 @@ object Remover {
   inline def apply[T, K](using r: Remover[T, K]): Remover.Aux[T, K, r.Out] = r
   inline def apply[T, K](t: T, k: K)(using r: Remover[T, K]): r.Out = r(t)
 
-  type RemoveField[T <: Tuple, K] = RemoveField0[T, K, EmptyTuple]
-
-  type RemoveField0[T <: Tuple, K, Acc <: Tuple] <: (Any, Tuple) = T match {
-    case (K ->> v) *: t => (v, ReversePrependTuple[Acc, t])
-    case h *: t => RemoveField0[t, K, h *: Acc]
+  type RemoveField[T <: Tuple, K] <: Tuple = T match {
+    case (K ->> _) *: t => t
+    case h *: t => h *: RemoveField[t, K]
   }
 
   inline given tupleRemover[T <: Tuple, K](
     using idx: ValueOf[FieldIndex[T, K]],
-  ): Remover.Aux[T, K, RemoveField[T, K]] =
+  ): Remover.Aux[T, K, (FieldValue[T, K], RemoveField[T, K])] =
     new Remover[T, K] {
-      type Out = RemoveField[T, K]
+      type Out = (FieldValue[T, K], RemoveField[T, K])
       def apply(t: T): Out = {
         val b = t.toArray.to(collection.mutable.Buffer)
         val v = b.remove(idx.value)
-        (v, Tuple.fromArray(b.to(Array))).asInstanceOf[RemoveField[T, K]]
+        (v, Tuple.fromArray(b.to(Array))).asInstanceOf[Out]
       }
     }
 }
